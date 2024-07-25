78aeaaed4fdfa3a14abf95dd8d17cb91d168bfbf
Clay Nicholson <clayanicholson@gmail.com>
2024-07-24 23:54:05 -0400

    Update

--- components/PopupForm.jsx
+++ components/PopupForm.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const PopupForm = ({ type, post, setPost, submitting, handleSubmit }) => {
 
   return (
-    <section className='flex items-center justify-center w-full max-w-full flex-start flex-col'>
+    <section className='flex items-center justify-center w-full max-w-full flex-start flex-col text-left'>
       <form
         onSubmit={handleSubmit}
         className='mt-10 w-full flex flex-col'
@@ -21,32 +21,30 @@ const PopupForm = ({ type, post, setPost, submitting, handleSubmit }) => {
         </label>
 
         <label>
-          <span className='font-satoshi font-semibold text-base text-gray-700'>
-            Your AI Prompt
+          <span className='font-satoshi font-semibold text-sm text-gray-400'>
+            Description
           </span>
 
           <textarea
             value={post.prompt}
             onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-            placeholder='Write your post here'
+            placeholder='Write your description here'
             className='form_textarea '
           />
         </label>
 
-        <label>
-          <span className='font-satoshi font-semibold text-base text-gray-700'>
-            Field of Prompt{" "}
-            <span className='font-normal'>
-              (#product, #webdevelopment, #idea, etc.)
-            </span>
+        <label className="mt-3">
+        <span className='font-satoshi font-semibold text-sm text-gray-400 p-2'>
+            Tags
           </span>
+
           <input
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
             type='text'
             placeholder='#Tag'
             required
-            className='form_input'
+            className='form_input_bottom'
           />
         </label>
 
@@ -55,7 +53,7 @@ const PopupForm = ({ type, post, setPost, submitting, handleSubmit }) => {
           <button
             type='submit'
             disabled={submitting}
-            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+            className='px-5 py-1.5 text-sm bg-green-700 rounded-full text-white'
           >
             {submitting ? `${type}ing...` : type}
           </button>
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,7 +18,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick, refresh}) 
     setShowPopup(!showPopup);
   };
 
-  const [copied, setCopied] = useState("");
 
   const cardRef = useRef(null);
 
@@ -53,11 +52,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick, refresh}) 
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
 
-  const handleCopy = () => {
-    setCopied(post.prompt);
-    navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(false), 3000);
-  };
+
   
 
   return (
@@ -88,19 +83,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick, refresh}) 
               {post.title}
             </h3>
           </div>
-        </div>
-
-        <div className='copy_btn' onClick={handleCopy}>
-          <Image
-            src={
-              copied === post.prompt
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
-            }
-            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
-            width={12}
-            height={12}
-          />
         </div>
       </div>
 
